b418dd5dcb53b1ad416edd797a24ffefde974738
Marc Tiehuis <marctiehuis@gmail.com>
2017-10-28 21:57:53 +1300

    Add faster arena example for binary-trees
    Runtime drops from ~24 seconds -> ~8 seconds. Some of the faster
    questions also use memory arenas so this might be legal???
    
    Side-note: Tested with jemalloc but this was marginally slower than
    standard malloc.

---
+++ binary-trees/binary-trees.arena.zig
@@ -0,0 +1,74 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const IncrementingAllocator = std.mem.IncrementingAllocator;
+const printf = std.io.stdout.printf;
+
+const TreeNode = struct {
+    l: ?&TreeNode,
+    r: ?&TreeNode,
+
+    pub fn new(a: &Allocator, l: ?&TreeNode, r: ?&TreeNode) -> &TreeNode {
+        var node = %%a.create(TreeNode);
+        node.l = l;
+        node.r = r;
+
+        node
+    }
+
+    pub fn free(self: &TreeNode, a: &Allocator) {
+        a.free(self);
+    }
+};
+
+fn itemCheck(node: &TreeNode) -> usize {
+    if (node.l) |left| {
+        // either have both nodes or none
+        1 + itemCheck(left) + itemCheck(??node.r)
+    } else {
+        1
+    }
+}
+
+fn bottomUpTree(a: &Allocator, depth: usize) -> &TreeNode {
+    if (depth > 0) {
+        TreeNode.new(a, bottomUpTree(a, depth - 1), bottomUpTree(a, depth - 1))
+    } else {
+        TreeNode.new(a, null, null)
+    }
+}
+
+pub fn main() -> %void {
+    const n = 21;
+
+    const min_depth: usize = 4;
+    const max_depth: usize = n;
+    const stretch_depth = max_depth + 1;
+
+    var inc_allocator = %%IncrementingAllocator.init(128 * 1024 * 1024);
+    defer inc_allocator.deinit();
+    var allocator = &inc_allocator.allocator;
+
+    const stretch_tree = bottomUpTree(allocator, stretch_depth);
+    _ = printf("depth {}, check {}\n", stretch_depth, itemCheck(stretch_tree));
+    inc_allocator.reset();
+
+    const long_lived_tree = bottomUpTree(allocator, max_depth);
+    const marker = inc_allocator.end_index;
+
+    var depth = min_depth;
+    while (depth <= max_depth) : (depth += 2) {
+        var iterations = usize(std.math.pow(f32, 2, f32(max_depth - depth + min_depth)));
+        var check: usize = 0;
+
+        var i: usize = 1;
+        while (i <= iterations) : (i += 1) {
+            const temp_tree = bottomUpTree(allocator, depth);
+            check += itemCheck(temp_tree);
+            inc_allocator.end_index = marker;
+        }
+
+        _ = printf("{} trees of depth {}, check {}\n", iterations, depth, check);
+    }
+
+    _ = printf("long lived tree of depth {}, check {}\n", max_depth, itemCheck(long_lived_tree));
+}
--- binary-trees/build.zig
+++ binary-trees/build.zig
@@ -2,7 +2,7 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: &Builder) {
     const mode = b.standardReleaseOptions();
-    const exe = b.addExecutable("binary-trees", "binary-trees.zig");
+    const exe = b.addExecutable("binary-trees", "binary-trees.arena.zig");
     exe.setBuildMode(mode);
 
     exe.linkSystemLibrary("c");
